899d5d141f73622f64759b2b991449ef33fbb8b7
saman <abdallaelsaman67@gmail.com>
2019-11-24 15:21:25 +0200

    Initial Commit

---
+++ Instagram_Clone_SW5/SignInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignInViewController.swift
+//  Instagram_Clone_SW5
+//
+//  Created by Abdalla Elsaman on 11/24/19.
+//  Copyright © 2019 Dumbies. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
