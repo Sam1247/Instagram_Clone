d7b818ffe5173352b176b87673279e69753f7e9c
saman <abdallaelsaman67@gmail.com>
2019-11-24 18:03:59 +0200

    Add user profile picture

--- Instagram_Clone_SW5/SceneDelegate.swift
+++ Instagram_Clone_SW5/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = SignInViewController()
+        window?.rootViewController = SignUpViewController()
         window?.makeKeyAndVisible()
     }
 
---
+++ Instagram_Clone_SW5/SignUpViewController.swift
@@ -0,0 +1,154 @@
+//
+//  SignInViewController.swift
+//  Instagram_Clone_SW5
+//
+//  Created by Abdalla Elsaman on 11/24/19.
+//  Copyright © 2019 Dumbies. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+
+class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // Views lazy init
+    let photoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "plus_photo"), for: .normal)
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
+        return button
+    }()
+
+    @objc func handlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editedImage = info[.editedImage] as? UIImage {
+            photoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            photoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+
+        photoButton.layer.cornerRadius = photoButton.frame.width / 2
+        photoButton.layer.masksToBounds = true
+
+        dismiss(animated: true, completion: nil)
+    }
+    
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(handleTextInputchange), for: .editingChanged)
+        return tf
+    }()
+    
+    let usernameTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Username"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(handleTextInputchange), for: .editingChanged)
+        return tf
+    }()
+
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.isSecureTextEntry = true
+        tf.addTarget(self, action: #selector(handleTextInputchange), for: .editingChanged)
+        return tf
+    }()
+    
+    @objc
+    func handleTextInputchange() {
+        let isFormValid = emailTextField.text?.count ?? 0 > 0 && usernameTextField.text?.count ?? 0 > 0
+            && passwordTextField.text?.count ?? 0 > 0
+        if isFormValid {
+            signUpButton.isEnabled = true
+            signUpButton.backgroundColor = UIColor.rgb(red: 17, green: 154, blue: 237)
+        } else {
+            signUpButton.isEnabled = false
+            signUpButton.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+        }
+    }
+    
+    @objc
+    func handleSignUp() {
+        guard let email = emailTextField.text else { return }
+        guard let username = usernameTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        guard let image = photoButton.imageView?.image else { return }
+        guard let imageData = image.jpegData(compressionQuality: 0.1) else { return }
+        Auth.auth().createUser(withEmail: email, password: password) { (res, error) in
+            if error != nil {
+                print(error.debugDescription)
+            }
+            let user = res?.user
+            let filename = NSUUID().uuidString
+            Storage.storage().reference().child("profile_images").child(filename).putData(imageData, metadata: nil) { (metadata, error) in
+                if error != nil {
+                    print(error.debugDescription)
+                }
+                let storageRef = Storage.storage().reference().child("profile_images").child(filename)
+                storageRef.downloadURL { (URL, error) in
+                    if error != nil {
+                        print(error.debugDescription)
+                    }
+                    guard let uid = user?.uid else { return }
+                    let profileImageUrl = URL?.absoluteString
+                    let databaseRef = Database.database().reference()
+                    let usersRef = databaseRef.child("users")
+                    let userRef = usersRef.child(uid)
+                    userRef.setValue(["username": username, "profileImageUrl": profileImageUrl])
+                }
+            }
+        }
+    }
+
+
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupInputFields()
+    }
+    
+    fileprivate func setupInputFields() {
+        view.addSubview(photoButton)
+        photoButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 40, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 140, height: 140)
+        photoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        let stackview = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField, signUpButton])
+        stackview.distribution = .fillEqually
+        stackview.axis = .vertical
+        stackview.spacing = 10
+        view.addSubview(stackview)
+        
+        stackview.anchor(top: photoButton.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 40, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 200)
+        
+    }
+}
+
